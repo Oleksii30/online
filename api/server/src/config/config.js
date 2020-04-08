633ab9f06b434adfb3d932819099e9f46559a209
Oleksii30 <polyanovskyyol@gmail.com>
2020-04-08 22:27:04 +0300

    babel scripts in pjson

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 
-const url = 'postgres://xuwxqejpjjsbhz:f5a753039e1f7f767993d8c012f280998de6f6bdedc138642ca6fa3e5ba4e5a7@ec2-35-174-88-65.compute-1.amazonaws.com:5432/d8ok0t8ab1aro5'
-
 module.exports = {
 
   // If using onine database
